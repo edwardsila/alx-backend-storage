6aa68dcacce43516338d7deefccb7aaf751ac6cf
edwardsila <edwardsila000@gmail.com>
2024-07-11 19:13:59 +0000

    best band ever!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,8 +1,5 @@
 -- ranks country origins of bands
 
-SOURCE /root/alx-backend-storage/0x00-MySQL_Advanced/metal_bands.sql
-
-CREATE VIEW temporary AS
 SELECT
     origin,
     SUM(fans) AS nb_fans
@@ -10,12 +7,5 @@ FROM
     metal_bands
 GROUP BY
     origin;
-
--- now select from the temporary table
-SELECT
-    origin,
-    nb_fans
-FROM
-    temporary
 ORDER BY
     nb_fans DESC;
