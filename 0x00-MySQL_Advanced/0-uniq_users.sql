f91ba275bf49778712dff63d35c9e005ec2bb914
edwardsila <edwardsila000@gmail.com>
2024-07-11 17:56:36 +0000

    we are all unique!

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,9 @@
 -- creates table users with:
 -- id, email and name
 
-CREATE TABLE IF NOT EXIST users(
-	id INT NOT NULL AUTO_INCREMENT,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255),
-	PRIMARY KEY (id)
-	);
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
