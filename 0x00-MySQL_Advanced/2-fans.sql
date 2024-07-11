339337e5f47c7af82bf99b3f61a3ad752ca987f7
edwardsila <edwardsila000@gmail.com>
2024-07-11 19:17:30 +0000

    best band ever!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 -- ranks country origins of bands
 
 SELECT
-    origin,
+    origin AS origin,
     SUM(fans) AS nb_fans
 FROM
     metal_bands
---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,3 @@
+-- list all bands with Glam rock as their main style
+
+SELECT 
