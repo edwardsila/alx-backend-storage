6a4ee6f71bd4890f3ffe2fb0731cc00eb642b86f
edwardsila <edwardsila000@gmail.com>
2024-07-11 19:21:44 +0000

    best band ever!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,11 +1,6 @@
 -- ranks country origins of bands
 
-SELECT
-    origin AS origin,
-    SUM(fans) AS nb_fans
-FROM
-    metal_bands
-GROUP BY
-    origin;
-ORDER BY
-    nb_fans DESC;
+SELECT origin AS origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
