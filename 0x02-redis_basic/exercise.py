0be783d653e2b23fe971cdab6da39f9427ca762c
edwardsila <edwardsila000@gmail.com>
2024-07-20 18:13:47 +0000

    reading from redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,8 @@ function that writes strings to redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional, Any
+
 
 class Cache:
     """ stores an instance of redis client """
@@ -19,3 +20,22 @@ class Cache:
 
         self._redis.set(key, data)
         return key
+
+    def get(
+      self,
+      key: str,
+      fn: Optional[Callable[[bytes], Any]] = None
+    ) -> Any:
+        value = self._redis.get(key)
+
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        return self.get(key, fn=int)
