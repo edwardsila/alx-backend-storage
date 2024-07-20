7eee45be6e6d2587efe5a90741334ad87edfcfa1
edwardsila <edwardsila000@gmail.com>
2024-07-20 16:14:59 +0000

    strings to redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+function that writes strings to redis
+"""
+import redis
+import uuid
+
+
+class Cache:
+    """ stores an instance of redis client """
+    def __init__(self):
+        self._redis = redis.Redis()
+
+        self._redis.flushdb()
+
+    def store(self, data: str) -> str:
+        """ generates random key """
+        key = str(uuid.uuid4())
+
+        self._redis.set(key, data)
+        return key
